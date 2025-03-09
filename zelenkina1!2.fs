3bef55bdff69014801743ebd318575f75fe3a7a9
Moranit <zelsvet2276@gmail.com>
2025-03-09 23:53:03 +0500

    zelenkina1!2.fs

--- zelenkina1!2.fs
+++ zelenkina1!2.fs
@@ -1,27 +1,29 @@
 open System
 
-// Функция для нахождения первой цифры натурального числа
-let firstDigit (n: int) =
-    if n <= 0 then
-        None // Возвращаем None, если число не натуральное
-    else
-        let numberString = n.ToString() // Преобразуем число в строку
-        Some (numberString.[0]) // Берем первый символ строки и возвращаем его
+// Функция для нахождения первой цифры натурального числа рекурсивно
+let rec firstd (n: int) =
+    if n < 10 then n //  если n меньше 10, то это первая цифра
+    else firstd (n / 10) //  иначе рекурсия
+
+let natchislo (n: int) =
+    n > 0
+
+// ввод числа
+let inputnumber () =
+    printfn "Введите число:"
+    let input = Console.ReadLine()
+    match Int32.TryParse(input) with
+    | (true, n) when natchislo n -> Some n 
+    | _ -> None // если ввод некорректен
 
 // Основная программа
 [<EntryPoint>]
 let main argv =
-    printfn "Введите натуральное число:"
-    let input = Console.ReadLine()
-
-    match Int32.TryParse(input) with
-    | (true, n) when n > 0 -> // Если введенное значение - натуральное число
-        match firstDigit n with
-        | Some digit ->
-            printfn "Первая цифра числа %d: %c" n digit
-        | None -> 
-            printfn "Ошибка: Некорректное значение."
-    | _ ->
+    match inputnumber () with
+    | Some n -> // если натуральное
+        let digit = firstd n //  первая цифра
+        printfn "Первая цифра числа %d: %d" n digit // Выводим первую цифру
+    | None ->
         printfn "Пожалуйста, введите корректное натуральное число."
 
     0 // Возвращаем код завершения программы
