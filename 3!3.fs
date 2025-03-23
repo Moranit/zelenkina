8d29e2c0be74a41dc9ae7f2b55bf42df910c5dee
Moranit <zelsvet2276@gmail.com>
2025-03-23 22:44:34 +0500

    3!3.fs

--- 3!3.fs
+++ 3!3.fs
@@ -1,24 +1,28 @@
 open System
 open System.IO
 
-let findShortestFileName (directoryPath: string) =
-    if not (Directory.Exists(directoryPath)) then
-        printfn "Каталог не существует."
+let rec getDirectoryPath () =
+    printfn "Введите путь к каталогу:"
+    let directoryPath = Console.ReadLine() 
+    if Directory.Exists(directoryPath) then
+        directoryPath 
     else
-        let files = Directory.GetFiles(directoryPath)
-        if files.Length = 0 then
-            printfn "В каталоге нет файлов."
-        else
-            let shortestFileName =
-                files
-                |> Seq.map Path.GetFileName 
-                |> Seq.minBy String.length 
-            printfn "Самое короткое название файла: %s" shortestFileName
+        printfn "Каталог не существует! Пожалуйста, попробуйте снова."
+        getDirectoryPath() 
+
+let findShortestFileName (directoryPath: string) =
+    let files = Directory.GetFiles(directoryPath)
+    if files.Length = 0 then
+        printfn "В каталоге нет файлов."
+    else
+        let shortestFileName =
+            files
+            |> Seq.map Path.GetFileName 
+            |> Seq.minBy String.length 
+        printfn "Самое короткое название файла: %s" shortestFileName
 
 [<EntryPoint>]
 let main argv =
-    printfn "Введите путь к каталогу:"
-    let directoryPath = Console.ReadLine() 
+    let directoryPath = getDirectoryPath() 
     findShortestFileName directoryPath
     0
-
