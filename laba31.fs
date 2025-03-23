fd868d4a38a38ffa229c0f951e01143b34b2fa33
Moranit <zelsvet2276@gmail.com>
2025-03-23 21:53:21 +0500

    laba31.fs

--- laba31.fs
+++ laba31.fs
@@ -1,49 +1,48 @@
-﻿open System
+open System
 
-// Функция для получения списка из консоли лаба 3/1
+// 3/1 нахожение мин цифры в числах
 let readListFromConsole () =
     let rec loop acc =
         printf "Введите элемент (или оставьте пустым для завершения): "
         let input = Console.ReadLine()
         if input = "" then
-            Seq.rev acc // возвращает новый список с элементами в обратном порядке
+            Seq.rev acc 
         else
-            // Проверяем, является ли введенное значение числом
             match Int32.TryParse(input) with
-            | (true, number) when number > 0 -> // Проверяем, что число натуральное (больше 0)
-                loop (number :: acc) // Добавляем введенное число к аккумулятору
+            | (true, number) when number > 0 -> 
+                loop (number :: acc) 
             | (true, number) when number <= 0 ->
                 printfn "Ошибка: '%s' не является натуральным числом." input
-                loop acc // Продолжаем ввод
+                loop acc 
             | (false, _) ->
                 printfn "Ошибка: '%s' не является числом." input
-                loop acc // продолжаем ввод
+                loop acc 
     loop []
 
 
-// Функция для вывода списка
+
 let printList seq =
     printfn "Содержимое списка:"
-    seq |> Seq.iter (fun element -> printfn "- %d" element) // Выводим число
+    seq |> Seq.iter (fun element -> printfn "- %d" element) 
 
-// Нахождение минимальной цифры
-let rec findMinDigit (number: int) (acc: int) =
-    if number = 0 then
-        acc
-    else
-        let digit = number % 10 // Получаем последнюю цифру
-        let newMin = if digit < acc then digit else acc // Обновляем минимальную цифру
-        findMinDigit (number / 10) newMin // Рекурсивный вызов, убирая последнюю цифру
 
-// Функция для нахождения минимальных цифр в последовательности чисел
+let findMinDigit (number: int) =
+    let rec loop number acc =
+        if number = 0 then
+            acc
+        else
+            let digit = number % 10 
+            let newMin = if digit < acc then digit else acc 
+            loop (number / 10) newMin 
+    loop number 9
+
+
 let findMinDigitsInSeq (numbers: seq<int>) =
-    numbers |> Seq.map (fun n -> findMinDigit n 9)
+    numbers |> Seq.map (fun n -> lazy (findMinDigit n))
 
-// Главная программа
 [<EntryPoint>]
 let main argv =
-    let numbers = readListFromConsole() // Читаем список чисел из консоли
-    // printList numbers // Выводим числа
-    let minDigits = findMinDigitsInSeq numbers // Находим минимальные цифры
-    printfn "Минимальные цифры в каждом числе: %A" minDigits // Выводим результаты
-    0 // Возвращаем код завершения
+    let numbers = readListFromConsole() 
+    let minDigits = findMinDigitsInSeq numbers 
+    printfn "Минимальные цифры в каждом числе: %A" (minDigits |> Seq.map (fun lazyValue -> lazyValue.Value)) 
+    0 
